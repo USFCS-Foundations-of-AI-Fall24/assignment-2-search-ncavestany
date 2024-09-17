67b2c01c9463cc15b820133b6f2f134972230a64
Noah Cavestany <ncavestany@dons.usfca.edu>
2024-09-16 19:52:37 -0700

    added state counters and depth limited search

--- search_algorithms.py
+++ search_algorithms.py
@@ -1,11 +1,10 @@
 from collections import deque
 
-
-
 ## We will append tuples (state, "action") in the search queue
 def breadth_first_search(startState, action_list, goal_test, use_closed_list=True) :
     search_queue = deque()
     closed_list = {}
+    state_counter = 0
 
     search_queue.append((startState,""))
     if use_closed_list :
@@ -20,22 +19,27 @@ def breadth_first_search(startState, action_list, goal_test, use_closed_list=Tru
             while ptr is not None :
                 ptr = ptr.prev
                 print(ptr)
+            print("Breadth first search state counter: ", state_counter)
             return next_state
         else :
             successors = next_state[0].successors(action_list)
+            state_counter += 1
             if use_closed_list :
                 successors = [item for item in successors
                                     if item[0] not in closed_list]
                 for s in successors :
                     closed_list[s[0]] = True
             search_queue.extend(successors)
+    print("Breadth first search state counter: ", state_counter)
+    
 
 ### Note the similarity to BFS - the only difference is the search queue
 
 ## use the limit parameter to implement depth-limited search
-def depth_first_search(startState, action_list, goal_test, use_closed_list=True,limit=0) :
+def depth_first_search(startState, action_list, goal_test, use_closed_list=True, limit=0) :
     search_queue = deque()
     closed_list = {}
+    stateCounter = 0
 
     search_queue.append((startState,""))
     if use_closed_list :
@@ -43,6 +47,10 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
     while len(search_queue) > 0 :
         ## this is a (state, "action") tuple
         next_state = search_queue.pop()
+
+        if limit > 0 and next_state[0].depth > limit:
+            continue
+        
         if goal_test(next_state[0]):
             print("Goal found")
             print(next_state)
@@ -50,18 +58,24 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
             while ptr is not None :
                 ptr = ptr.prev
                 print(ptr)
+            print("Depth first search state counter: ", stateCounter)
             return next_state
         else :
             successors = next_state[0].successors(action_list)
+            stateCounter += 1
             if use_closed_list :
                 successors = [item for item in successors
                                     if item[0] not in closed_list]
                 for s in successors :
                     closed_list[s[0]] = True
+            for s in successors: # Increment the depth of the successors
+                s[0].depth = next_state[0].depth + 1
             search_queue.extend(successors)
+            
+    # If the search finishes without finding the goal
+    print("Depth first search state counter: ", stateCounter)
 
-## add iterative deepening search here
 
 
 
-f
+
