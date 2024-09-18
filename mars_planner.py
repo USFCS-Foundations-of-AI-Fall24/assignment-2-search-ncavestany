3380bb0271c4c95840d561d265a308fc60aa2ccf
Noah Cavestany <ncavestany@dons.usfca.edu>
2024-09-17 19:30:27 -0700

    added extra tests

--- mars_planner.py
+++ mars_planner.py
@@ -142,8 +142,23 @@ def mission_complete(state) :
 if __name__=="__main__" :
     s = RoverState()
     bfs_result = breadth_first_search(s, action_list, mission_complete)
-    print("\n\n\n")
+    print("BFS result: ", bfs_result, "\n\n")
+    
     dfs_result = depth_first_search(s, action_list, mission_complete)
+    print("DFS result: ", dfs_result, "\n\n")
+    
+    dls_result = depth_first_search(s, action_list, mission_complete, limit=5)
+    print("DLS result: ", dls_result, "\nLimit: 5 \n\n")
+    
+    move_to_sample_result = breadth_first_search(s, action_list, sample_goal) # Trying to get to the sample from the start
+    print("Move to sample result: ", move_to_sample_result, "\n\n")
+    
+    remove_sample_result = breadth_first_search(move_to_sample_result[0], action_list, station_goal) # Trying to remove the sample while at the sample
+    print("Remove sample result: ", remove_sample_result, "\n\n")
+    
+    return_to_charger_result = breadth_first_search(remove_sample_result[0], action_list, battery_goal) # Trying to return to the charger after removing the sample
+    print("Return to charger result: ", return_to_charger_result, "\n\n")
+    
 
 
 
